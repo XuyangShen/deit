5bbea6d416114805c144ef7471ec2b33b5abaeb7
Xuyang Shen <xuyangshen@outlook.com>
2023-11-28 10:09:13 +0000

    Clean repo

---
+++ env.sh
@@ -0,0 +1,7 @@
+# CUDA 11.6
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.6 -c pytorch -c nvidia
+# CUDA 11.7
+conda install pytorch==1.13.1 torchvision==0.14.1 torchaudio==0.13.1 pytorch-cuda=11.7 -c pytorch -c nvidia
+
+
+pip install timm==0.3.2
